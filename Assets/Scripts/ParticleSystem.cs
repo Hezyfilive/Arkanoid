08440947b452974a092467c1cf562cf7c16e1fb4
Hezyfilive <59973241+Hezyfilive@users.noreply.github.com>
2023-08-16 11:31:49 +0300

    Update ParticleSystem.cs

--- Assets/Scripts/ParticleSystem.cs
+++ Assets/Scripts/ParticleSystem.cs
@@ -4,8 +4,6 @@ public class NewBehaviourScript : MonoBehaviour
 {
     private void Start()
     {
-        //Change Foreground to the layer you want it to display on 
-        //You could prob. make a public variable for this
         gameObject.GetComponent<Renderer>().sortingLayerName = "Score";
     }
 }
